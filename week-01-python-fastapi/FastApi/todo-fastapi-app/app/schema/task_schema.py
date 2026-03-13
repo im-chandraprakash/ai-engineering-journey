2e3c511c43aac948c2cbc863e261592e2e6ede2a
Chandraprakash <07sahuprakash@gmail.com>
2026-03-14 00:53:09 +0530

    setting up the infra

---
+++ week-01-python-fastapi/FastApi/todo-fastapi-app/app/schema/task_schema.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel 
+
+class TaskCreate(BaseModel) :
+    title : str
+
+class TaskResponse(BaseModel) : 
+    id : int
+    title : str
+    completed : bool
---
+++ week-01-python-fastapi/FastApi/todo-fastapi-app/app/services/task_service.py
@@ -0,0 +1,6 @@
+from app.database.memory_store import tasks
+from app.models.task_model import Task
+
+
+def get_all_tasks() : 
+    return tasks
